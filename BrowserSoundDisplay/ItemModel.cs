66ca3b2be65f232ed14ae068ba6172fea7bc2474
eon <eoinraeside393@gmail.com>
2021-02-06 18:28:13 +0000

    Initial commit

---
+++ BrowserSoundDisplay/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BrowserSoundDisplay
+{
+    public class ItemModel
+    {
+        public string Url { get; set; }
+        public string ImageName { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
---
+++ BrowserSoundDisplay/SettingsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserSoundDisplay
+{
+    public class SettingsModel
+    {
+        public string BackgroundColour { get; set; }
+        public string TextFont { get; set; }
+        public string TextColour { get; set; }
+        public string TextSize { get; set; }
+        public string MusicBrowser { get; set; }
+        public List<ItemModel> PlaylistInfo { get; set; }
+    }
+}
